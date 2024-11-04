8285972a61f09a157c8755b73a98615b5268c4b9
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2024-11-04 11:18:36 +0545

    basic login finish

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -27,6 +27,45 @@ class UserController {
       res.status(500).json({ success: false, message: "Server Error" });
     }
   }
+
+  static async login(req: Request, res: Response) {
+    //Accept incoming data
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({
+        success: false,
+        message: "Please provide email and password!",
+      });
+      return;
+    }
+
+    //check email exist or not at first
+    const [user] = await User.findAll({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      res
+        .status(404)
+        .json({ success: false, meessage: "No user with that Email 🥹🥹" });
+    } else {
+      //if yes-->email exist ->check password too
+      const isEqual = bcrypt.compareSync(password, user.password);
+
+      if (!isEqual) {
+        res
+          .status(404)
+          .json({ success: false, message: "Invalid password 🥹🥹" });
+      } else {
+        //if password milyo vane -->token generate(jwt)
+        res
+          .status(200)
+          .json({ success: true, message: "Login successful! 🥳🥳" });
+      }
+    }
+  }
 }
 
 export default UserController;
